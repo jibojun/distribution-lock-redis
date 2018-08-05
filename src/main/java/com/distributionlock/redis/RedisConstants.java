a84d2ac63f6a8727127ddc47d792a8e7949eb991
jibojun <jbj19910123@163.com>
2018-08-06 01:01:41 +0800

    update

--- src/main/java/com/distributionlock/redis/RedisConstants.java
+++ src/main/java/com/distributionlock/redis/RedisConstants.java
@@ -6,24 +6,24 @@ package com.distributionlock.redis;
  * @Date: 2018/8/6_12:50 AM
  */
 public class RedisConstants {
-    private static final int MAX_TOTAL = 20;
+    public static final int MAX_TOTAL = 20;
 
-    private static final int MAX_IDLE = 10;
+    public static final int MAX_IDLE = 10;
 
-    private static final int MIN_IDLE = 5;
+    public static final int MIN_IDLE = 5;
 
-    private static final String REDIS_HOST="127.0.0.1";
+    public static final String REDIS_HOST="127.0.0.1";
 
-    private static final int REDIS_CONN_TIME_OUT=5000;
+    public static final int REDIS_CONN_TIME_OUT=5000;
 
-    private static final int REDIS_PORT=6379;
+    public static final int REDIS_PORT=6379;
 
-    private static final String REDIS_CONN_PWD="";
+    public static final String REDIS_CONN_PWD="";
 
-    private static final boolean REDIS_TEST_ON_BORROW=true;
+    public static final boolean REDIS_TEST_ON_BORROW=true;
 
-    private static final boolean REDIS_TEST_ON_RETURN=false;
+    public static final boolean REDIS_TEST_ON_RETURN=false;
 
-    private static final boolean REDIS_BLOCK_WHEN_EXHAUSTED=true;
+    public static final boolean REDIS_BLOCK_WHEN_EXHAUSTED=true;
 
 }
--- src/main/java/com/distributionlock/redis/RedisPool.java
+++ src/main/java/com/distributionlock/redis/RedisPool.java
@@ -1,6 +1,8 @@
 package com.distributionlock.redis;
 
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * @Author: Bojun Ji
@@ -11,15 +13,33 @@ public class RedisPool {
 
     private static JedisPool pool;
 
+    /**
+     * init a redis connection pool with configuration assigned in constants
+     */
     public static void initRedisPool(){
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(RedisConstants.MAX_TOTAL);
+        config.setMaxIdle(RedisConstants.MAX_IDLE);
+        config.setMinIdle(RedisConstants.MIN_IDLE);
+        config.setTestOnBorrow(RedisConstants.REDIS_TEST_ON_BORROW);
+        config.setTestOnReturn(RedisConstants.REDIS_TEST_ON_RETURN);
+        config.setBlockWhenExhausted(RedisConstants.REDIS_BLOCK_WHEN_EXHAUSTED);
         pool=new JedisPool();
     }
 
-    public static JedisPool getPool() {
+    private static JedisPool getPool() {
         return pool;
     }
 
-    public static void setPool(JedisPool pool) {
+    private static void setPool(JedisPool pool) {
         RedisPool.pool = pool;
+    }
+
+    public static Jedis getConnResource(){
+        return pool.getResource();
+    }
+
+    public static void closeConnResource(Jedis jedis){
+        jedis.close();
     }
 }
