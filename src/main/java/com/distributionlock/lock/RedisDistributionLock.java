ce75e35515efebc553066c8363f04792627ff1b6
jibojun <jbj19910123@163.com>
2018-08-07 23:34:30 +0800

    update

--- src/main/java/com/distributionlock/lock/RedisDistributionLock.java
+++ src/main/java/com/distributionlock/lock/RedisDistributionLock.java
@@ -1,5 +1,7 @@
 package com.distributionlock.lock;
 
+import redis.clients.jedis.Jedis;
+
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -8,6 +10,21 @@ import java.util.concurrent.TimeUnit;
  * @Date: 2018/8/6_11:23 PM
  */
 public class RedisDistributionLock extends AbstractDistributionLock {
+    //connection resource
+    private Jedis jedis;
+    //lock name, also the ket for redis cache
+    protected String lockName;
+    //expire time for the lock
+    protected long lockExpireTime;
+
+    private RedisDistributionLock(){}
+
+    public RedisDistributionLock(Jedis jedis, String lockName, long lockExpireTime){
+        this.jedis=jedis;
+        this.lockName=lockName;
+        this.lockExpireTime=lockExpireTime;
+    }
+
     @Override
     public boolean isLocked() {
         return false;
